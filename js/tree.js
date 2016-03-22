714bf01a235e8afb6f864c5f3e022dce0f5dc543
U-HIK\lilei <lilei@NB-PUBLICGXRJ19.hikvision.com>
2016-03-22 17:01:40 +0800

    更换树控件

---
+++ js/tree.js
@@ -0,0 +1,80 @@
+var fs = require("fs");
+
+$(function(){
+	// --- Initialize first Dynatree -------------------------------------------
+	$("#tree").dynatree({
+		//children: data,
+		onActivate: function(node) {
+			//$("#echoActive").text(node.data.title + "(" + node.data.key + ")");
+		},
+		onDeactivate: function(node) {
+			//$("#echoActive").text("-");
+		},
+		dnd: {
+			onDragStart: function(node) {
+				//logMsg("tree.onDragStart(%o)", node);
+				return true;
+			},
+			onDragStop: function(node) {
+				//logMsg("tree.onDragStop(%o)", node);
+			},
+			autoExpandMS: 1000,
+			preventVoidMoves: true, // Prevent dropping nodes 'before self', etc.
+			onDragEnter: function(node, sourceNode) {
+				if (node.data.isFolder) {
+					return true;
+				};
+				return false;
+			},
+			onDragOver: function(node, sourceNode, hitMode) {
+				//logMsg("tree.onDragOver(%o, %o, %o)", node, sourceNode, hitMode);
+				if(node.isDescendantOf(sourceNode)){
+					return false;
+				}
+			},
+			onDrop: function(node, sourceNode, hitMode, ui, draggable) {
+				//logMsg("tree.onDrop(%o, %o, %s)", node, sourceNode, hitMode);
+				if (!node.data.isFolder) {
+					draggable = false;
+				}else{
+					sourceNode.move(node, hitMode);
+					if (!node.bExpanded) {
+						node.toggleExpand();
+					};
+				}
+			},
+			onDragLeave: function(node, sourceNode) {
+				//logMsg("tree.onDragLeave(%o, %o)", node, sourceNode);
+			}
+		}
+	});
+	var rootNode = $("#tree").dynatree("getRoot");
+	walk("./doc",rootNode);
+});
+
+
+function walk(path,parentNode){
+    var files = fs.readdirSync(path);
+    for(fn in files){
+        var fname = path + "/" + files[fn];  
+         var stat = fs.lstatSync(fname);  
+         if(parentNode){
+
+         }
+         //console.log(files[fn]);  
+         //var menu = Menu(parentNode).addSubMenu(files[fn],"fa fa-table fa-fw");
+         if(stat.isDirectory() == true){  
+         	var childNode = parentNode.addChild({
+				title: files[fn],
+				isFolder: true, 
+				expand: true 
+				});
+	        walk(fname,childNode);  
+		}else{
+			parentNode.addChild({
+				title: files[fn],
+				isFolder: false
+			});
+         }
+    }
+}
